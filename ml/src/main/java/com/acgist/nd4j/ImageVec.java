8820de80ba0b7570b4af47b008e1b5f0329f59b6
acgist <289547414@qq.com>
2018-06-11 10:11:21 +0800

    + 性别识别

---
+++ ml/src/main/java/com/acgist/nd4j/ImageVec.java
@@ -0,0 +1,19 @@
+package com.acgist.nd4j;
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.split.InputStreamInputSplit;
+import org.datavec.image.recordreader.ImageRecordReader;
+
+public class ImageVec {
+
+	public static void main(String[] args) {
+		try(RecordReader reader = new ImageRecordReader(750, 500)) {
+			reader.initialize(new InputStreamInputSplit(ImageVec.class.getResourceAsStream("/image/a.jpg")));
+			System.out.println(reader.hasNext());
+			System.out.println(reader.next());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
